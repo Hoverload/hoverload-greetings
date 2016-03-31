c81df51ae4e34d2b362175473bb629ab4997f9f3
Vineet Kumar Doshi <vineetkumardoshi@users.noreply.github.com>
2016-04-01 01:00:11 +0530

    reverted to original

--- sketch.js
+++ sketch.js
@@ -42,17 +42,8 @@ function mousePressed() {
 
 function touchMoved(){
     noStroke();
-    if (touchY < can_height/3) {
-      fill(255,map(touchY, 0, can_height/3, 153,255),map(touchY, 0, can_height/3, 51,255));
-    }
-    else if (touchY > can_height/3*2){
-      fill(map(touchY, can_height/3*2, can_height, 255,0),map(touchY, can_height/3*2, can_height, 255, 100),map(touchY, can_height/3*2, can_height, 255, 8));
-    }
-    ellipse(touchX+random(-10,10), touchY+random(-10,10), can_height/10, can_height/10);
-    ellipse(touchX+random(-20,10), touchY+random(-20,10), can_height/10, can_height/10);
-    ellipse(touchX+random(-10,20), touchY+random(-10,20), can_height/10, can_height/10);
-    ellipse(touchX+random(-30,10), touchY+random(-30,10), can_height/10, can_height/10);
-    ellipse(touchX+random(-10,30), touchY+random(-10,30), can_height/10, can_height/10);
+    fill(map(touchX, 0, can_width, 150, 200),0,map(touchY, 0, can_height, 150, 255));
+    ellipse(touchX, touchY, can_height/3, can_height/3);
 }
 
 function draw(){
@@ -60,17 +51,8 @@ function draw(){
   if(toggle%2 == 0){
     
     noStroke();
-    if (mouseY < can_height/3) {
-      fill(255,map(mouseY, 0, can_height/3, 153,255),map(mouseY, 0, can_height/3, 51,255));
-    }
-    else if (mouseY > can_height/3*2){
-      fill(map(mouseY, can_height/3*2, can_height, 255,0),map(mouseY, can_height/3*2, can_height, 255, 100),map(mouseY, can_height/3*2, can_height, 255, 8));
-    }
-    ellipse(mouseX+random(-10,10), mouseY+random(-10,10), can_height/10, can_height/10);
-    ellipse(mouseX+random(-20,10), mouseY+random(-20,10), can_height/10, can_height/10);
-    ellipse(mouseX+random(-10,20), mouseY+random(-10,20), can_height/10, can_height/10);
-    ellipse(mouseX+random(-30,10), mouseY+random(-30,10), can_height/10, can_height/10);
-    ellipse(mouseX+random(-10,30), mouseY+random(-10,30), can_height/10, can_height/10);
+    fill(map(mouseX, 0, can_width, 150, 200),0,map(mouseY, 0, can_height, 150, 255));
+    ellipse(mouseX, mouseY, can_height/3, can_height/3);
     cursor(CROSS);
   } else {
     cursor(HAND);
