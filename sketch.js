8324e81024d48e430958cc9f1f9264a7b67ab664
Vineet Kumar Doshi <vineetkumardoshi@users.noreply.github.com>
2016-04-19 16:32:20 +0530

    Edited the welcome page ( index.html )

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var URL = window.location.href;
 var URL_CLEAN = URL.match(/#.*/);
 console.log(URL_CLEAN);
 if(URL_CLEAN === null){
-  URL_CLEAN = "#SGVsbG8gVXNlciUyM1VSTCBsaW5rIGlzIGluY29ycmVjdCUyM0FkbWlu";
+  URL_CLEAN = "#SGVsbG8gVXNlciwlMjNXZWxjb21lISBUbyBIb3ZlcmxvYWQlMjNBZG1pbg==";
 }
 URL_CLEAN = URL_CLEAN.toString().substring(1);
 var URL_DECODE = window.atob(URL_CLEAN);
